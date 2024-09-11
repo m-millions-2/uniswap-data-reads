db2ebcd720fa5bfcbd88e19abfe352b6d65cd797
m-millions-2 <ventresca.code@gmail.com>
2024-09-11 02:58:18 -0400

    Update request_call.py
    Sets up global variables and token id iteration

--- request_call.py
+++ request_call.py
@@ -2,13 +2,39 @@ import json
 import requests
 
 
-TOKEN_ID = '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'
+TOKENS = [
+          '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599',
+          '0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce',
+          '0x6810e776880c02933d47db1b9fc05908e5386b96' 
+         ]
 
-def query_subgraph():
+URI = 'https://gateway.thegraph.com/api/[INSERT API KEY HERE]/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV'
+
+
+def query_subgraph(Token):
+
+    json_data = { 
+                 "query": "{ token(id: \"" + Token + "\") { id name symbol } }", 
+                 "operationName": "Subgraphs", 
+                 "variables": {} 
+                }
+
+    response = requests.post(URI, json=json_data)  
+    
+    # Check if the request was successful
+    # ToDo: There is a small bug in this error checkin logic,
+    #       which will be addressed in a later committ
+    if response.status_code == 200:
+        print(response)
+        return response.json()
+    else:
+        print("Error:", response.text)
+        return None
+
+
+def main():
     '''
-    This is the first iteration of a simple function, which queries a Uniswap Subgraph.  
-
-    The original Curl construct is:
+    This function queries a Uniswap Subgraph.  The original Curl construct is:
 
     Curl -X POST \
       -H "Content-Type: application/json" \
@@ -30,31 +56,11 @@ def query_subgraph():
                   }
         }
     '''
+    for Token in TOKENS:
+        result = query_subgraph(Token)
+        if result:
+            print(json.dumps(result, indent=4))
 
-    response = requests.post(uri, json=json_data)  
-    
-    # Check if the request was successful
-    # ToDo: There is a small bug in this error checkin logic,
-    #       which will be addressed in a later committ
-    if response.status_code == 200:
-        print(response)
-        return response.json()
-    else:
-        print("Error:", response.text)
-        return None
-
-uri = 'https://gateway.thegraph.com/api/[Insert API KEY Here]/subgraphs/id/5zvR82QoaXYFyDEKLZ9t6v9adgnptxYpKpSbxtgVENFV'
-
-json_data = { 
-       "query": "{ token(id: \"" + TOKEN_ID + "\") { id name symbol } }", 
-       "operationName": "Subgraphs", 
-       "variables": {} 
-       }
-
-def main():
-  result = query_subgraph()
-  if result:
-    print(json.dumps(result, indent=4))
 
 if __name__ == "__main__":
   main()
